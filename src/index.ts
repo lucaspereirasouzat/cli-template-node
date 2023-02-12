757441e8001ae0fc8e383f5c77fd88eef691de15
Lucas <lucas.souza@edulabzz.com.br>
2023-02-12 16:24:48 -0300

    feat: update controller factory

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,21 @@ const program = new Command()
 program
   .name('clean_code_template_cli')
   .description('CLI based on template clean arquiteture')
-  .version('0.0.24')
+  .version('0.0.34')
 
 program
   .command('create <name>')
   .description('Create a new file based on a template')
   .option('-test, --tests', 'Create test')
+  .option('-onlyTest', '--onlyTest', 'Run only test')
+  .option('-pro', '--properties', 'Properties')
   .option('-cta, --contract', 'Create Contract')
+  .option('-err, --error', 'Create error')
   .option('-ctl, --controller', 'Create Controller')
   .option('-use, --useCase', 'Create UseCases')
   .option('-gat, --gateWay', 'Create Gateway')
   .option('-rep, --repo', 'Create repository')
   .option('-ent, --entity', 'Create entity')
-  .option('-err, --error', 'Create error')
-  .option('-pro', '--properties', 'Properties')
-  .option('-onlyTest', '--onlyTest', 'Run only test')
   .action((name, options) => {
     console.log('entrou')
 
