25e2c535149377e0cc15fda840ba1faa9d3b386e
Lucas <lucas.souza@edulabzz.com.br>
2023-02-17 10:24:03 -0300

    fix: merge conflic

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@
 import { Command } from "commander";
 import packajson from "./package.json";
 import adapter from "./main/adapter";
-import latestVersion from "latest-version";
+// import latestVersion from "latest-version";
 
-(async () => {
-	const version = await latestVersion("clean_code_template_cli");
-	if (version !== packajson.version) {
-		console.log("new version avaible");
-	}
-})();
+// (async () => {
+// 	const version = await latestVersion("clean_code_template_cli");
+// 	if (version !== packajson.version) {
+// 		console.log("new version avaible");
+// 	}
+// })();
 
 const program = new Command();
 program
