8236636dabb399f20e429a9d639a5045aec2a0b5
Lucas <lucas.souza@edulabzz.com.br>
2023-02-20 23:46:50 -0300

    feat: otimization

--- src/main/router.ts
+++ src/main/router.ts
@@ -1,16 +1,16 @@
-import { makeContract, makeController, makeEntity, makeError, makeGateway, makeUseCase, makeRepository, makeValidation, makeRoute } from '../factories/domain/use-cases'
+import { makeContract, makeController, makeEntity, makeError, makeGateway, makeUseCase, makeRepository, makeValidation, makeRoute, makeMiddleware } from '../factories/domain/use-cases'
 
 export default () => {
   return {
-    controller: makeController(),
-    useCase: makeUseCase(),
-    repo: makeRepository(),
-    gateWay: makeGateway(),
-    error: makeError(),
-    entity: makeEntity(),
-    contract: makeContract(),
-    validation: makeValidation(),
-    route: makeRoute(),
-    midleware: () => {}
-  }
+			controller: makeController(),
+			useCase: makeUseCase(),
+			repo: makeRepository(),
+			gateWay: makeGateway(),
+			error: makeError(),
+			entity: makeEntity(),
+			contract: makeContract(),
+			validation: makeValidation(),
+			route: makeRoute(),
+			midleware: makeMiddleware()
+		};
 }
