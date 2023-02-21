73102290ee6f9a303d75e0777bf9bc5494261b5a
Lucas <lucas.souza@edulabzz.com.br>
2023-02-20 23:30:44 -0300

    feat: otimization

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,41 +1,39 @@
-export const PATH_CONTROLLER = '../../resources/views/templates/Controller.html'
-export const PATH_CONTROLLER_TEST =
-  '../../resources/views/templates/ControllerTest.html'
-export const PATH_CONTROLLER_APLICATION = 'application/controllers'
+export const PATH_CONTROLLER = "../../resources/views/templates/Controller.html";
+export const PATH_CONTROLLER_TEST = "../../resources/views/templates/ControllerTest.html";
+export const PATH_CONTROLLER_APLICATION = "application/controllers";
 
-export const CONTROLLER_FACTORY_PATH = 'main/factories/application/controllers'
-export const PATH_FACTORY_CONTROLLER =
-  '../../resources/views/templates/FactoryController.html'
+export const CONTROLLER_FACTORY_PATH = "main/factories/application/controllers";
+export const PATH_FACTORY_CONTROLLER = "../../resources/views/templates/FactoryController.html";
 
-export const DOMAIN_CONTRACT_PATH = 'domain/contracts'
-export const PATH_CONTRACT = '../../resources/views/templates/Contract.html'
+export const DOMAIN_CONTRACT_PATH = "domain/contracts";
+export const PATH_CONTRACT = "../../resources/views/templates/Contract.html";
 
-export const PATH_ERROR = '../../resources/views/templates/Error.html'
-export const PATH_ERROR_APLICATION = 'domain/entities/errors'
+export const PATH_ERROR = "../../resources/views/templates/Error.html";
+export const PATH_ERROR_APLICATION = "domain/entities/errors";
 
-export const PATH_USE_CASE_DOMAIN = 'domain/use-cases'
-export const PATH_USE_CASE_GATEWAY = 'main/factories/domain/use-cases'
-export const PATH_USE_CASE = '../../resources/views/templates/UseCase.html'
-export const PATH_USE_CASE_TEST = '../../resources/views/templates/UseCaseTest.html'
-export const PATH_USE_CASE_FACTORY = '../../resources/views/templates/FactoryUseCase.html'
+export const PATH_USE_CASE_DOMAIN = "domain/use-cases";
+export const PATH_USE_CASE_GATEWAY = "main/factories/domain/use-cases";
+export const PATH_USE_CASE = "../../resources/views/templates/UseCase.html";
+export const PATH_USE_CASE_TEST = "../../resources/views/templates/UseCaseTest.html";
+export const PATH_USE_CASE_FACTORY = "../../resources/views/templates/FactoryUseCase.html";
 
-export const GATEWAY_PATH_APLICATION = 'infra/gateways'
-export const PATH_GATEWAY = '../../resources/views/templates/Gateways.html'
-export const PATH_GATEWAY_TEST = '../../resources/views/templates/GatewaysTest.html'
-export const GATEWAY_FACTORY_PATH = 'main/factories/infra/gateways'
+export const GATEWAY_PATH_APLICATION = "infra/gateways";
+export const PATH_GATEWAY = "../../resources/views/templates/Gateways.html";
+export const PATH_GATEWAY_TEST = "../../resources/views/templates/GatewaysTest.html";
+export const GATEWAY_FACTORY_PATH = "main/factories/infra/gateways";
 
-export const PATH_FACTORY_GATEWAY = '../../resources/views/templates/FactoryGateway.html'
+export const PATH_FACTORY_GATEWAY = "../../resources/views/templates/FactoryGateway.html";
 
-export const PATH_ENTITY_PATH = 'domain/entities'
-export const PATH_ENTITY = '../../resources/views/templates/Entity.html'
-export const PATH_ENTITY_TEST = '../../resources/views/templates/EntityTest.html'
+export const PATH_ENTITY_PATH = "domain/entities";
+export const PATH_ENTITY = "../../resources/views/templates/Entity.html";
+export const PATH_ENTITY_TEST = "../../resources/views/templates/EntityTest.html";
 
-export const PATH_REPOSITORY = '../../resources/views/templates/Repository.html'
-export const PATH_REPOSITORY_TEST = '../../resources/views/templates/RepositoryTest.html'
-export const PATH_FACTORY_REPOSITORY = '../../resources/views/templates/RepositoryFactory.html'
+export const PATH_REPOSITORY = "../../resources/views/templates/Repository.html";
+export const PATH_REPOSITORY_TEST = "../../resources/views/templates/RepositoryTest.html";
+export const PATH_FACTORY_REPOSITORY = "../../resources/views/templates/RepositoryFactory.html";
 
-export const REPOSITORY_PATH = 'infra/repos/postgres'
-export const REPOSITORY_FACTORY_PATH = 'main/factories/infra/repos/postgres'
+export const REPOSITORY_PATH = "infra/repos/postgres";
+export const REPOSITORY_FACTORY_PATH = "main/factories/infra/repos/postgres";
 
 export const PATH_VALIDATION = "../../resources/views/templates/Validation.html";
 export const PATH_VALIDATION_TEST = "../../resources/views/templates/ValidationTest.html";
@@ -44,3 +42,11 @@ export const VALIDATION_PATH = "application/validation";
 export const PATH_ROUTE = "../../resources/views/templates/Route.html";
 export const PATH_ROUTE_TEST = "../../resources/views/templates/RouteTest.html";
 export const ROUTE_PATH = "main/routes";
+
+export const PATH_MIDDLEWARE = "../../resources/views/templates/Middleware.html";
+export const PATH_MIDDLEWARE_TEST = "../../resources/views/templates/MiddlewareTest.html";
+export const MIDDLEWARE_PATH = "application/middlewares";
+
+export const MIDDLEWARE_MAIN_PATH = "main/middlewares";
+export const PATH_MAIN_MIDDLEWARE = "../../resources/views/templates/MiddlewareMain.html";
+
